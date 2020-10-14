54c898d3d4074019077c430d8cd05322da23d8ba
Vova Vorobiev <vovabandit1945@gmail.com>
2020-10-14 23:07:20 +0300

    урок 2

--- js/script.js
+++ js/script.js
@@ -1,10 +1,21 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
-const  cash = 1;
-alert(cash);
+let money = 10;
+let income = 'Фриланс';
+let addExpenses ='Интернет, Такси, Телефон' ;
+let deposit =  true;
+let mission = 100;
+let period = 5;
 
-console.log('цыфвы');
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать' + mission + 'рублей');
+let miniAddExpenes = addExpenses.toLowerCase();
+console.log(miniAddExpenes);
+let arrayExpenes = [miniAddExpenes.substring(0,8), miniAddExpenes.substring(10,15),miniAddExpenes.substring(17,24)];
+console.log(arrayExpenes);
+let budgetDay = money / 30;
+console.log(budgetDay);
+
+
