71577739cef50fae8ee9af6d6ee8818da51f0357
Vova Vorobiev <vovabandit1945@gmail.com>
2020-10-17 10:42:39 +0300

    урок3/0

--- js/script.js
+++ js/script.js
@@ -5,18 +5,15 @@ let addExpenses ='Интернет, Такси, Телефон' ;
 let deposit =  true;
 let mission = 100;
 let period = 5;
-/*
+
 console.log(typeof money);
 console.log(typeof income);
 console.log(typeof deposit);
 console.log(addExpenses.length);
 console.log('Период равен ' + period + ' месяцев');
 console.log('Цель заработать ' + mission + ' рублей');
-let miniAddExpenes = addExpenses.toLowerCase();
-console.log(miniAddExpenes);
+console.log(addExpenses.toLowerCase().split(', '));
 
-console.log(miniAddExpenes.split(', '));
-*/
 let budgetDay = money / 30;
 /*
 let num = 266219;
@@ -26,14 +23,14 @@ let sumNumPow = sumNumber ** 3;
 let sumConsole = sumNumPow.toString();
 console.log(sumConsole.substr(0,2));*/
 
-money= prompt('Ваш месячный доход?');
+money= +prompt('Ваш месячный доход?');
 addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
 deposit = confirm('Есть у вас депозит в банке?');
 let expenses1 = prompt('Введите обязательную статью расходов?');
-let amount1 = prompt('Восколько это обойдется?');
+let amount1 = +prompt('Восколько это обойдется?');
 let expenses2 = prompt('Введите обязательную статью расходов?');
-let amount2 = prompt('Восколько это обойдется?');
-let budgetMonth = +amount1 + parseInt(amount2);
+let amount2 = +prompt('Восколько это обойдется?');
+let budgetMonth = amount1 + amount2;
 console.log(budgetMonth);
 
 console.log( Math.ceil(mission/(money - budgetMonth)));
